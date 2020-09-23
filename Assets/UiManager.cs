65adeecac84d467b6527b7e81af8eb1d2ca95d1c
DeekshitaV <deekshitaverma@gmail.com>
2020-09-24 00:46:26 +0530

    completed!

---
+++ Assets/UiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public GameObject[] filters;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filters[0].SetActive(true);
+    }
+
+    public void onButtonPressed( GameObject filter )
+    {
+        foreach( var f in filters)
+        {
+            f.SetActive(false);
+        }
+        filter.SetActive(true);
+    }
+    // Update is called once per frame
+    
+        
+    
+}
---
+++ Assets/scripts/LifecycleManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class LifecycleManager : MonoBehaviour
+{
+    [SerializeField] ARSession m_Session;
+
+    IEnumerator Start()
+    {
+        if (ARSession.state == ARSessionState.None || ARSession.state == ARSessionState.CheckingAvailability)
+        {
+            yield return ARSession.CheckAvailability();
+        }
+
+        if (ARSession.state == ARSessionState.Unsupported)
+        {
+            m_Session.enabled = false;
+            // Start some fallback experience for unsupported devices
+            _ShowAndroidToastMessage("We're sorry, your device is unsupported with this application");
+            Invoke("_DoQuit", 0.5f);
+        }
+        else
+        {
+            // Start the AR session
+            m_Session.enabled = true;
+            //EventBroker.FireShowHandUIEvent();
+        }
+    }
+
+    void Update()
+    {
+        UpdateApplicationLifecycle();
+    }
+
+    void UpdateApplicationLifecycle()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        //Screen.sleepTimeout = ARSession.state != ARSessionState.SessionTracking ? SleepTimeout.SystemSetting : SleepTimeout.NeverSleep;
+
+        if (ARSession.state != ARSessionState.SessionTracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+    }
+
+    void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
